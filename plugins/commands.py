6127ab459aac68880616f16ff305bc7fbd7e40f5
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-02-21 11:12:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,8 +50,8 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('search here movie', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton('updates', url='https://t.me/josprojects'),
-            InlineKeyboardButton('movie club', url='https://t.me/+y53tWFUw6Q43NzE9')
+            InlineKeyboardButton('updates', url='https://t.me/ss_linkz'),
+            InlineKeyboardButton('movie club', url='https://t.me/netflixmovies_sslinkz')
             ],[
             InlineKeyboardButton('✗ Close the Menu ✗', callback_data='close_data')
         ]]
@@ -98,8 +98,8 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('search here movie', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton('updates', url='https://t.me/josprojects'),
-            InlineKeyboardButton('movie club', url='https://t.me/+y53tWFUw6Q43NzE9')
+            InlineKeyboardButton('updates', url='https://t.me/ss_linkz'),
+            InlineKeyboardButton('movie club', url='https://t.me/netflixmovies_sslinkz')
             ],[
             InlineKeyboardButton('✗ Close the Menu ✗', callback_data='close_data')
         ]]
@@ -146,11 +146,11 @@ async def start(client, message):
                 f_caption = f"{title}"
             buttons = [
                 [
-                    InlineKeyboardButton('support', url='https://t.me/JOSPSupport'),
-                    InlineKeyboardButton('channel', url='https://t.me/JosProjects')
+                    InlineKeyboardButton('support', url='https://t.me/ss_linkz'),
+                    InlineKeyboardButton('channel', url='https://t.me/netflixmovies_sslinkz')
                 ],
                 [
-                    InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/+y53tWFUw6Q43NzE9')
+                    InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/ss_linkz')
                 ]
                 ]
             try:
@@ -260,11 +260,11 @@ async def start(client, message):
         f_caption = f"{files.file_name}"
     buttons = [
         [
-            InlineKeyboardButton('support', url='https://t.me/JOSPSupport'),
+            InlineKeyboardButton('support', url='https://t.me/ss_linkz'),
             InlineKeyboardButton('channel', url='https://t.me/JosProjects')
         ],
         [
-            InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/+y53tWFUw6Q43NzE9')
+            InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/ss_linkz')
         ]
         ]
     await client.send_cached_media(
